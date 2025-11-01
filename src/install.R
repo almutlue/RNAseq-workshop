1451387fd8e2b01f9d360f62af5bb612c57e95bb
almutlue <almut.lue@gmail.com>
2025-11-01 22:54:59 +0100

    docs: first draft R basics presentation

--- src/install.R
+++ src/install.R
@@ -1,3 +1,3 @@
 install.packages(c("BiocManager", "remotes"))
-BiocManager::install(c("SummarizedExperiment", "AnnotationDbi", 
+BiocManager::install(c("SummarizedExperiment", "airway", "AnnotationDbi", 
                        "org.Mm.eg.db", "DESeq2"))
